104cf32faaefd79255ced58ee5b13044241d96cb
Michael Nelson Jr <michaeljr.nelson@gmail.com>
2014-05-27 21:15:33 -0400

    Test

--- study/src/main/java/com/nelson/flashcards/study/Welcome.java
+++ study/src/main/java/com/nelson/flashcards/study/Welcome.java
@@ -14,6 +14,8 @@ public class Welcome extends ActionBarActivity {
         setContentView(R.layout.activity_welcome);
     }
 
+    //test
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
