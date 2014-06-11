3c296b76469f40e35defda04efc05a3f3243b92b
Michael Nelson Jr <michaeljr.nelson@gmail.com>
2014-06-11 07:08:47 -0400

    Added remove, edit, and add new question to NoteCard activity

--- study/src/main/java/com/nelson/flashcards/study/NoteCard.java
+++ study/src/main/java/com/nelson/flashcards/study/NoteCard.java
@@ -1,5 +1,7 @@
 package com.nelson.flashcards.study;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.ActionBarActivity;
@@ -8,7 +10,10 @@ import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -58,10 +63,13 @@ public class NoteCard extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.question_add:
+                displayPopUp("Add a Question and Answer","Enter the Question","Enter the Answer", 1 );
                 break;
             case R.id.question_edit:
+                displayPopUp("Edit a Question and Answer",questions.get(index),answers.get(index), 2);
                 break;
             case R.id.question_delete:
+                displayPopUp("Delete this Question and Answer",questions.get(index),answers.get(index), 3);
                 break;
             default:
                 break;
@@ -80,6 +88,91 @@ public class NoteCard extends ActionBarActivity {
             }
         }
         db.close();
+    }
+
+    public void displayPopUp(String title, String qPrompt, String aPrompt, final int addDelUp){
+        AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+        LinearLayout linLayout = new LinearLayout(this);
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        popUp.setTitle(title);
+
+        //TextView and EditText for question add/edit
+        TextView qText = new TextView(this);
+        qText.setText(qPrompt);
+        linLayout.addView(qText);
+        final EditText questEdit = new EditText(this);
+        questEdit.setHint(qPrompt);
+        linLayout.addView(questEdit);
+
+        //TextView and EditText for answer add/edit
+        TextView aText = new TextView(this);
+        aText.setText(aPrompt);
+        linLayout.addView(aText);
+        final EditText answerEdit = new EditText(this);
+        answerEdit.setHint(aPrompt);
+        linLayout.addView(answerEdit);
+        popUp.setView(linLayout);
+
+        popUp.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                switch (addDelUp) {
+                    case 1:
+                        addQuestion(questions.get(index), answers.get(index), known.get(index));
+                        Toast.makeText(getApplicationContext(), "Added", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        editQuestion((long)index + 1, questions.get(index), answers.get(index), known.get(index));
+                        Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        deleteQuestion((long)index + 1);
+                        Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        popUp.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String result = "Cancelled, Nothing Changed";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        popUp.show();
+    }
+
+    public void addQuestion (String q, String a, int k) {
+        db.open();
+        db.insertRecord(q, a, k);
+        db.close();
+        questions.add(q);
+        answers.add(a);
+        known.add(0);
+        size++;
+    }
+
+    public void editQuestion (long idx, String q, String a, int k) {
+        db.open();
+        db.updateRecord(idx, q, a ,k);
+        db.close();
+        questions.set(index, q);
+        answers.set(index, a);
+        known.set(index, k);
+    }
+
+    public void deleteQuestion (long idx) {
+        db.open();
+        db.deleteRecord(idx);
+        db.close();
+        questions.remove(index);
+        answers.remove(index);
+        known.remove(index);
+        size--;
     }
 
     @Override
